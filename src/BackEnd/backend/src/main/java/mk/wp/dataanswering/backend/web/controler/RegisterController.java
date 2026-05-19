68ade8a2191cce42eb3e89e05667ea200b18909f
Antonio Trajkovski <antonio.trajkovski.1@students.finki.ukim.mk>
2026-05-19 16:16:41 +0200

    Updated Log in + log out + register

---
+++ src/BackEnd/backend/src/main/java/mk/wp/dataanswering/backend/web/controler/RegisterController.java
@@ -0,0 +1,48 @@
+package mk.wp.dataanswering.backend.web.controler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import lombok.AllArgsConstructor;
+import mk.wp.dataanswering.backend.model.enums.Role;
+import mk.wp.dataanswering.backend.service.RegisteredUserService;
+
+@Controller
+@RequestMapping("/register")
+@AllArgsConstructor
+public class RegisterController {
+    
+    private final RegisteredUserService registeredUserService;
+
+    @GetMapping
+    public String getRegisterPage(Model model) {
+        model.addAttribute("bodyContent", "register");
+        return "master-template";
+    }
+
+    @PostMapping
+    public String register(@RequestParam String username,
+                           @RequestParam String userFirstName,
+                           @RequestParam String userLastName,
+                           @RequestParam String userEmail,
+                           @RequestParam String password,
+                           @RequestParam String repeatPassword,
+                           Model model) {
+
+        try {
+            this.registeredUserService.register(username, userFirstName, userLastName, userEmail, password,repeatPassword, Role.ROLE_USER);
+            return "redirect:/login";
+        } catch (RuntimeException e) {
+            model.addAttribute("error", e.getMessage());
+            model.addAttribute("bodyContent", "register");
+            return "master-template";
+        }
+
+    }
+
+    
+}
